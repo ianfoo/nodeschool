496cf611c64af9207f9f4b1c3fdfef97b679831b
Ian Molee <ian@metricstory.com>
2018-10-01 11:13:39 -0700

    learnyoureact: Add properties and state

--- learnyoureact/views/index.jsx
+++ learnyoureact/views/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default class TodoBox extends React.Component {
 	render() {
@@ -14,13 +15,47 @@ export default class TodoBox extends React.Component {
 
 class TodoList extends React.Component {
 	render() {
-		return (
+	        return (
 			<div className="todoList">
-				I am a TodoList.
+				<table style={{border: "2px solid black;"}}>
+					<tbody>
+						<Todo title="Shopping">Milk</Todo>
+						<Todo title="Hair cut">13:00</Todo>
+						<Todo title="Learn React">15:00</Todo>
+					</tbody>
+			</table>
 			</div>
 		);
 	}
 }
+
+class Todo extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {checked: false};
+	}
+
+	handleChange(e) {
+		this.setState({checked: e.target.checked});
+	}
+
+	render() {
+		return (
+			<tr>
+				<td style={{border: "1px solid black;"}}>
+					<input type="checkbox" checked={this.state.checked} onChange={this.handleChange.bind(this)}/>
+				</td>
+				<td style={{border: "1px solid black;"}}>{this.props.title}</td>
+				<td style={{border: "1px solid black;"}}>{this.props.children}</td>
+			</tr>
+		);
+	}
+
+}
+Todo.propTypes = {
+	title: PropTypes.string.isRequired
+
+};
 
 class TodoForm extends React.Component {
 	render() {
@@ -31,21 +66,3 @@ class TodoForm extends React.Component {
 		);
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
